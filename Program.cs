cf13d4b49c19ac094914a9a5984bbb50d000b603
Arashkkazemifar <160579576+Arashkkazemifar@users.noreply.github.com>
2024-05-26 18:26:50 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class tavan2
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter a number");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(tavan(number));
+        Console.WriteLine("press any key to continue");
+        Console.ReadKey();
+    }
+    public static int tavan(int number)
+    {
+        if (number == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 2 * tavan(number - 1);
+        }
+    }
+}
